6433b994bd94042cbff1f3858908ed23a8d0d34e
everdimension <everdimension@gmail.com>
2016-03-31 17:51:50 +0300

    update indentation; add readme

---
+++ README.md
@@ -0,0 +1,12 @@
+## Angular svg base fix
+
+A small directive for fixing SVG `xlink:href` within a document with a base tag
+
+
+### How to use
+
+```html
+<svg>
+  <use svg-base-fix xlink:href="#icon-name"></use>
+</svg>
+```
--- src/svgBaseFix.js
+++ src/svgBaseFix.js
@@ -1,48 +1,51 @@
 (function (root, factory) {
-	'use strict';
-	if (typeof define === 'function' && define.amd) {
-		// AMD. Register as an anonymous module.
-		define(['angular'], factory);
-	} else if (typeof module !== 'undefined' && typeof module.exports === 'object') {
-		// CommonJS support (for us webpack/browserify/ComponentJS folks)
-		module.exports = factory(require('angular'));
-	} else {
-		// in the case of no module loading system
-		// then don't worry about creating a global
-		// variable like you would in normal UMD.
-		// It's not really helpful... Just call your factory
-		return factory(root.angular);
-	}
+  'use strict';
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define(['angular'], factory);
+  } else if (typeof module !== 'undefined' && typeof module.exports === 'object') {
+    // CommonJS support (for us webpack/browserify/ComponentJS folks)
+    module.exports = factory(require('angular'));
+  } else {
+    // in the case of no module loading system
+    // then don't worry about creating a global
+    // variable like you would in normal UMD.
+    // It's not really helpful... Just call your factory
+    return factory(root.angular);
+  }
 }(this, function (angular) {
-	'use strict';
+  'use strict';
 
-	var moduleName = 'svgBaseFix';
-	angular.module(moduleName, [])
+  var moduleName = 'svgBaseFix';
+  angular.module(moduleName, [])
 
-		.directive('svgBaseFix', ['$rootScope', function($rootScope) {
-			return {
-				restrict: 'A',
-				link: function(scope, element, attrs) {
-					var attr = 'xlinkHref';
+    .directive('svgBaseFix', ['$rootScope', function($rootScope) {
+      return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+          var attr = 'xlinkHref';
 
-					var initialUrl = attrs[attr];
-					var parsingNode = document.createElement('a');
+          var initialUrl = attrs[attr];
+          var parsingNode = document.createElement('a');
 
-					attrs.$observe(attr, updateValue);
-					$rootScope.$on('$locationChangeSuccess', updateValue);
+          attrs.$observe(attr, updateValue);
+          $rootScope.$on('$locationChangeSuccess', updateValue);
 
-					function updateValue() {
-						var newVal;
-						parsingNode.setAttribute('href', location.pathname + location.search + initialUrl);
-						newVal = parsingNode.toString();
+          function updateValue() {
+            var newVal;
+            parsingNode.setAttribute(
+              'href',
+              location.pathname + location.search + initialUrl
+            );
+            newVal = parsingNode.toString();
 
-						if (newVal && attrs[attr] !== newVal) {
-							attrs.$set(attr, parsingNode.toString());
-						}
-					}
-				}
-			};
-		}]);
+            if (newVal && attrs[attr] !== newVal) {
+              attrs.$set(attr, parsingNode.toString());
+            }
+          }
+        }
+      };
+    }]);
 
-	return moduleName;
+  return moduleName;
 }));
